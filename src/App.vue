78c5771be6697840997aff86be8449f4171971fc
annettethomas <42778997+annettethomas@users.noreply.github.com>
2018-12-04 13:04:25 -0700

    final commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="container">
+        <!--ToDo: Load app-header-->
+        <app-header></app-header>
+        <div class="row">
+            <div class="col-xs-12">
+                <transition name="slide" mode="out-in">
+                    <!--ToDo: Load router-view-->
+                    <router-view></router-view>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    //ToDo: Import Header Component
+    import Header from './components/Header.vue'
+
+    export default {
+        //ToDo: Initialize Header component and name it appHeader
+        components: {
+            appHeader: Header
+        },
+        //ToDo: On page creation dispatch 'initStocks' to the vuex $store
+
+        created(){
+            this.$store.dispatch('initStocks');
+        }
+
+
+
+    }
+</script>
+
+<style>
+    body {
+        padding: 30px;
+    }
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    .slide-leave-active {
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: rotateX(0deg);
+        }
+
+        to {
+            transform: rotateX(360deg);
+        }
+    }
+
+    @keyframes slide-out {
+
+        from {
+            transform: rotateY(0deg);
+        }
+
+        to {
+            transform: rotateY(360deg);
+        }
+
+    }
+    /*ToDo: Create slide-in effect for enter-active*/
+    /*ToDo: Create slide-in effect for leave-active*/
+
+    /*ToDo: Create Keyframes for slide-in, remember to include from and to*/
+        /*You can use any settings*/
+
+    /*ToDo: Create Keyframes for slide-out, remember to include from and to*/
+        /*You can use any settings*/
+
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,48 @@
+//ToDo: Import vue and name it Vue
+//ToDo: Import vue-router and name it VueRouter
+//ToDo: Import vue-resource and name it VueResource
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+
+//ToDo: Import App.vue and name it App
+//ToDo: Import routes.js and use the {} passing routes
+//ToDo: Import store/store.js and name it store
+
+import  App from './App.vue'
+import { routes } from './routes'
+import store from './store/store'
+
+
+//ToDo Initialize VueRouter using Vue.use()
+//ToDo Initialize VueResource using Vue.use()
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://stocktrader-14bb4.firebaseio.com/';
+//ToDo: Set Equal to your Firebase Database URL
+
+Vue.filter('currency', (value) => {
+  //ToDo: Return value.toLocaleString(), Add $ sign before the value
+    return '$' + value.toLocaleString();
+
+});
+
+const router = new VueRouter({
+    //ToDo: Set router mode to history
+    //ToDo: Pass routes constant imported from above
+    mode: 'history',
+    routes,
+});
+
+new Vue({
+  el: '#app',
+    //ToDo: Pass the router constant to vue application
+    //ToDo: Pass store constant to vue application,
+    router,
+    store,
+  render: h => h(App)
+});
